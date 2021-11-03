dcc0cd438259949aa09623e4a999ab78eafe3941
sjhuang <hsjfans_scholar@163.com>
2021-11-03 16:26:22 +0800

    add

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-
+online=$1
 save_path='checkpoints'
 if [ ! -d ${save_path} ];then
    mkdir ${save_path}
@@ -8,4 +8,8 @@ else
    echo '${save_path} exits'
 fi
 
-python train.py --t '/Users/sjhuang/Documents/docs/dataset/train' --s ${save_path}
+if [ ${online} == "linux" ];then
+   python train.py --t '/Disk/hsj/dataset/train' --s ${save_path}
+else
+   python train.py --t '/Users/sjhuang/Documents/docs/dataset/train' --s ${save_path}
+fi
